e6c3299c131328732f57edfc2e8313e9eb0a724e
Gabriel <gaplogan@gmail.com>
2021-04-11 10:03:17 -0300

    2021.04.11.10.03

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace cs_1._23_AreaTriangulo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            double b, h, area;
+            
+            Console.Write("Digite o valor da base: ");
+            b = double.Parse(Console.ReadLine());
+            // Garante base positiva.
+            b = Math.Abs(b);
+
+            Console.Write("Digite o valor da altura: ");
+            h = double.Parse(Console.ReadLine());
+            // Garante altura positiva.
+            h = Math.Abs(h);
+
+            // Fórmula da área do triângulo
+            area = (b * h) / 2;
+
+            Console.WriteLine($"\nÁrea do triângulo: {area}");
+        }
+    }
+}
